d0d6870c869ad67e7714a2b1220ac756cb9d5905
radityasurya <radityasurya@hotmail.com>
2018-07-09 13:00:06 +0200

    Add element-ui framework

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,58 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Global CSS
+   */
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+
+  /*
+   ** Add element-ui in our app, see plugins/element-ui.js file
+   */
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+  /*
+   ** Headers of the page
+   */
   head: {
     title: 'xkc',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Xebia Knowledge Center' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Xebia Knowledge Center'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: '#3B8070'
+  },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
+     ** Run ESLint on save
+     */
+    extend(config, {
+      isDev,
+      isClient
+    }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
