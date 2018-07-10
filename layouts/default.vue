a2a9717545fd70995a59d3d477d6bf419bee3f37
radityasurya <radityasurya@hotmail.com>
2018-07-10 02:58:32 +0200

    Refactor default layout to use sidebar topics state

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,35 @@
 <template>
     <div class="app">
       <app-header></app-header>
-      <!-- <el-container>
+      <el-container>
         <el-aside>
-          sidebar
+          <ul>
+            <h3>Topics</h3>
+            <li v-for="(topic, index) in topics" :key="index">
+              <a href="#">{{topic}}</a>
+            </li>
+          </ul>
         </el-aside>
-        <el-main>
+        <main class="main">
           <nuxt/>
-        </el-main>
+        </main>
       </el-container>
-      <el-footer><p>Designed and Coded by <a href="https://radityasurya.com/">Raditya Surya Pratama</a></p></el-footer> -->
     </div>
 </template>
 
 
 <script>
 import header from "~/components/Header";
+import { mapGetters } from "vuex";
+
 export default {
   components: {
     "app-header": header
+  },
+  computed: {
+    ...mapGetters({
+      topics: "topics/get"
+    })
   }
 };
 </script>
@@ -33,17 +44,38 @@ body {
   background: #f9f9f9;
 }
 
-.el-aside {
-  background-color: #d3dce6;
-  width: 200px;
+.el-container {
+  max-width: 1100px;
+  padding-left: 15px;
+  padding-right: 15px;
+  -webkit-box-sizing: border-box;
   box-sizing: border-box;
-  flex-shrink: 0;
+  margin-left: auto;
+  margin-right: auto;
+  min-width: 320px;
+  margin-top: 30px !important;
 }
 
-.el-main {
+.el-aside {
+  width: 100px;
+  box-sizing: border-box;
+  flex-shrink: 0;
+  height: 200px;
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    li {
+      margin: 0 0 10px 0;
+    }
+  }
+}
+
+.main {
   background-color: #e9eef3;
   color: #333;
-  text-align: center;
-  line-height: 160px;
+  width: 900px;
+  height: 100px;
 }
 </style>
