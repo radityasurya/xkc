4274fb3ed352bf9e9fb65ba5e976e60732849b4c
radityasurya <radityasurya@hotmail.com>
2018-07-12 01:56:49 +0200

    Use warning component if state is empty

---
+++ components/TopicList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="topics">
+    <div v-if="isEmpty">
+      <warning message="No topics available right now!"></warning>
+    </div>
+  </div>
+</template>
+
+<script>
+import warning from "~/components/Warning";
+
+export default {
+  components: {
+    warning
+  },
+  computed: {
+    isEmpty: function() {
+      return true;
+    }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style>
