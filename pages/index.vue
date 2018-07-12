fd50aa67d388b5b05273e110f0f57fe0e33f7914
radityasurya <radityasurya@hotmail.com>
2018-07-12 05:59:28 +0200

    Load selected topic repo when page mounted

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,28 @@
 <template>
-  <section>
-    <div>
-      Homepage
-    </div>
-  </section>
+  <el-row>
+    <el-col>
+      <repo-list></repo-list>
+    </el-col>
+  </el-row>
 </template>
 
+
+<script>
+import RepoList from "~/components/RepoList.vue";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "HomePage",
+  components: {
+    RepoList
+  },
+  computed: {
+    ...mapGetters({
+      selected: "topics/getSelectedTopic"
+    })
+  },
+  mounted() {
+    this.$store.dispatch("repos/getRepos", this.selected);
+  }
+};
+</script>
